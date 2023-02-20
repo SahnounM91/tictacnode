a82bdb2ddadb0b87d03639b45040634f337bcce0
sahnounM91 <sahnoun.mabrouk@gmil.com>
2023-02-20 19:37:41 +0100

    place player marks

--- scene.js
+++ scene.js
@@ -9,4 +9,20 @@ export class Scene {
             chalk.yellow('X')
         ]
     }
+
+    // place X or O in a position
+
+    place(player) {
+        const position = player.playerMove(this)
+
+        if (this.table[position] == 0) {
+            this.table[position] = player.turn
+            return true
+        }
+        console.log('place not free !!')
+        return false
+    }
+
+
+    
 }
